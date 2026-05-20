6c4d17f2e55203d124bd1ddc48ab58dde3baa938
supremealpha04-sudo <supremealpha04@gmail.com>
2026-05-20 08:23:33 -0700

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,7 @@
-
 import { createBrowserClient } from '@supabase/ssr'
-import { Database } from '@/types/supabase'
 
 export const createClient = () =>
-  createBrowserClient<Database>(
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
